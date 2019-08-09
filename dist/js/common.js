054744126e476def5c6c4e93e92bbe25cc5c71af
cheolmin-s <scmjazz@navevr.com>
2019-08-09 16:50:04 +0900

    js로 수정

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,277 +1,78 @@
-var init = (function(){
+const init = function(){
 
-    var $window = $(window);
-    var $body = $('html,body');
+    const html = document.querySelector('html,body');
+    const CLASS_ON = "on";
 
-    // GNB
-    var gnbActive = function(){
+    const gnb = function(){
         
-        var $header = $('.common-header');
-        var $gnbDepth1 = $('.depth1 > li > a');
-        var $lang = $('.lang');
-        var $gnbDepth2 = $('.depth2 > li > a')
+        const header = document.querySelector('.common-header');
+        const depth1 = document.querySelectorAll('.depth1 > li > a');
+        const depth2 = document.querySelectorAll('.depth2 > li > a');
+        const lang = document.querySelector('.lang');
 
-        
-        $gnbDepth1.on('mouseenter focus', function(){
-
-            $gnbDepth1.addClass('on');
-            $header.addClass('on');
-
-        });
-
-        $header.on('mouseleave', function(){
-
-            $gnbDepth1.removeClass('on');
-            $header.removeClass('on');
-
-        });
-
-        // 2뎁스 마지막 링크 포커스 헤더 열림
-        $gnbDepth2.last().on('focus', function(){
-
-            $gnbDepth1.addClass('on');
-            $header.addClass('on');
-        });
-
-        // 헤더 영역 벗어나면 헤더 닫힘
-        $lang.on('blur', function(){
-
-            $gnbDepth1.removeClass('on')
-            $header.removeClass('on');
-
-        });
-    };
-
-    // Main 비디오 
-    var mainVideo = function(){
-        
-        var $videoBtn = $('.video-btn > button');
-
-        $videoBtn.on('click',function(){
-
-            var $this = $(this);
-
-            $(this).toggleClass('on');
-
-            if($(this).hasClass('on')){
-                
-                $('#main-video').get(0).pause();    
-                $(this).text('동영상 재생');                
-
-            } else {
-
-                $('#main-video').get(0).play();
-                $(this).text('동영상 일시정지');
-
-            }
-        });
-    };
-
-    // Newsroom 버튼
-    var newsRoom = function(){
-
-        var $newsRoomBtn = $('.news-room button');
-
-        $newsRoomBtn.on('click',function(){
-
-            $('.content, .news-room button').toggleClass('on');
-            
-        });
-
-    };
-
-    // LNB 메뉴
-    var lnbBtn = function(){
-
-        var $lnbButton = $('.lnb button');
-
-        $lnbButton.on('click',function(e){
-
-            var $this = $(this);
-
-            e.preventDefault();
-
-            if( $this.hasClass('on')){
-
-                $this.removeClass('on').next().removeClass('on');
-            } else {
-
-                $lnbButton.removeClass('on').next().removeClass('on');
-                $this.addClass('on').next().addClass('on');
-            }
-        });
-    };
-
-    // 스크롤 효과
-    var scrollActive = function(){
-        $window.on('scroll', scrollEvent);
-
-        function scrollEvent(){
-
-            var $active = $('.scroll-active');
-            var scrollTop = $window.scrollTop() + $window.height() - 200;
-
-            $active.each(function(){
-
-                var $this = $(this);
-                var offset = $(this).offset().top;
-
-                if( scrollTop >= offset){
-                    $this.addClass('on');
-                } else {
-                    $this.removeClass('on');
-                }
-            });
-
+        function handleMouseLeave(){
+            header.classList.remove(CLASS_ON);
         }
-        scrollEvent();
-    };
 
-    // TOP 이동
-    var topBtn = function(){
-
-        var $topBtn = $('.top-btn');
-        var offSet = $body.offset().top;
-
-        $window.on('scroll', scrollBtnEvent);
-
-        function scrollBtnEvent(){
-
-            var scrollTop = $window.scrollTop();
-
-            if(scrollTop > 0){
-                $topBtn.addClass('on');
-
-                // 여기 수정 해야된다.
-                if ( scrollTop > 2865){
-                    $topBtn.css('bottom',168);
-                } else {
-                    $topBtn.css('bottom',21);
-                }
-
-            } else {
-                $topBtn.removeClass('on');
-            }
+        function handleMouseEnter(){
+            header.classList.add(CLASS_ON);
         }
-        scrollBtnEvent();
 
-        $topBtn.on('click', function(e){
-
-            e.preventDefault();
-
-            $body.stop().animate({scrollTop : offSet},500);
+        depth1.forEach(list => {
+            list.addEventListener('focus', handleMouseEnter);
+            list.addEventListener('mouseenter', handleMouseEnter);
         });
-    };
 
-    // history list 메뉴
-    var historyNav = function(){
+        header.addEventListener('mouseleave', handleMouseLeave);
 
-        var $historyNavLink = $('.history-list-nav a');
-        var $sec = $('.sec'); 
-        var $historyNavList = $('.history-list-nav');
+        lang.addEventListener('blur', handleMouseLeave);
+
+        depth2[depth2.length - 1].addEventListener('focus', handleMouseEnter);
+
+    }
+
+    const mainVideo = function(){
+
+        const video = document.querySelector('#main-video');
+        const btn = document.querySelector('.video-btn button');
+
+        function videoToggle(){
+
+            if( btn.classList.contains(CLASS_ON)){
+                video.play();    
+                btn.classList.remove(CLASS_ON);  
+                btn.innerText = "동영상 일시정지";
+            } else {
+                video.pause();
+                btn.classList.add(CLASS_ON);
+                btn.innerText = "동영상 재생";    
+            }               
+        }
+
+        btn.addEventListener('click', videoToggle);
+    }
+
+    const newsRoom = function(){
         
-        $historyNavLink.on('click',function(e){
+        const btn = document.querySelector('.news-room button');
+        const content = document.querySelector('.news-room .content');
 
-            e.preventDefault();
-            e.stopPropagation();
+        function handleClick(){
+            btn.classList.toggle(CLASS_ON);
+            content.classList.toggle(CLASS_ON);
+        }
+
+        btn.addEventListener('click', handleClick);
+    }
+
     
-            var $this = $(this);
-            var index = $this.parent().index();
-            var position = $sec.eq(index).offset().top - 234;
-
-            $body.stop().animate({scrollTop:position},600);
-
-            // $historyNavLink.removeClass('on');   
-            // $this.addClass('on');
-    
-        });
-
-
-        $window.on('scroll', scrollEvent);
-
-        function borderBottom(){
-
-            var scrollTop = $window.scrollTop();
-
-            $historyNavList.each(function(){
-
-                var $this = $(this);
-                var offSet = $this.offset().top - 71;
-
-                if( scrollTop >= offSet ){
-                    $this.addClass('on');
-                    
-                } else {
-                    $this.removeClass('on');
-                }
-            })
-
-        }
-
-        function scrollColor(){
-
-            var scrollTop = $window.scrollTop();
-
-            $sec.each(function(){
-
-                var $this = $(this);
-                var index = $this.index() - 2;
-                var position = $this.offset().top - 235;
-
-                if( scrollTop >= position){
-
-                    $historyNavLink.removeClass('on');
-                    $historyNavLink.eq(index).addClass('on');
-
-                }
-
-            })
-
-        }
-
-        function scrollEvent(){
-            borderBottom();
-            scrollColor();
-        };
-        
-    };
-
-    // FAQ 아코디언
-    var arcordionList = function(){
-
-        var $contents = $('.faq-list dd'); 
-        var $btn = $('.faq-list button');
-
-        $btn.on('click',function(){
-
-            var $this = $(this);
-            var index = $this.parents('dl').index();
-
-            if($this.hasClass('on')){
-                $contents.eq(index).stop().slideUp();
-                $this.removeClass('on');
-
-            } else{
-                $btn.removeClass('on');
-                $contents.stop().slideUp();
-                $this.addClass('on');
-                $contents.eq(index).stop().slideDown();
-            }
-        });
-    };
- 
-
-    // init
-    gnbActive();
+    //실행
+    gnb();
     mainVideo();
-    newsRoom();
-    lnbBtn();
-    scrollActive();
-    topBtn();
-    historyNav();
-    arcordionList();
-});
+    newsRoom(); 
 
-// 실행
-$(init);
+};
+
+init();
+
 //# sourceMappingURL=../maps/common.js.map
